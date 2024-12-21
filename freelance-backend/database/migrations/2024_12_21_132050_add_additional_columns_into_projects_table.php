21491e1f85c88148cf8f7b7559821e100646ad16
Aleksandra Motic <ana.motic@gmail.com>
2024-12-21 14:49:16 +0100

    Kreirane migracije za MySQL tabele

---
+++ freelance-backend/database/migrations/2024_12_21_132050_add_additional_columns_into_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('deadline')->nullable()->after('budget');
+            $table->enum('priority', ['low', 'medium', 'high'])->default('low');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('deadline');
+            $table->dropColumn('priority');
+        });
+    }
+};
