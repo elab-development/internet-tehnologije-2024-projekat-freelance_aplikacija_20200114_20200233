52e6920c022c956232bf8119fa819546773bc495
Marija Mihajlović <marijam00111@gmail.com>
2024-12-22 17:24:13 +0100

    Dodate rute i njihova logika pozivanja uvedena

--- freelance-backend/routes/api.php
+++ freelance-backend/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\RequestController;
+use App\Http\Controllers\ReviewController;
 
 Route::post('/register', [AuthController::class, 'register']); 
 Route::post('/login', [AuthController::class, 'login']); 
@@ -8,17 +11,22 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
 
     Route::prefix('ponudjac')->group(function () {
-        Route::get('/moji-projekti', [ProjectController::class, 'showMyProjects']); 
-        Route::post('/projekti', [ProjectController::class, 'store']); 
-        Route::put('/projekti/{id}', [ProjectController::class, 'update']); 
-        Route::patch('/projekti/{id}/budzet', [ProjectController::class, 'updateBudget']); 
-        Route::delete('/projekti/{id}', [ProjectController::class, 'destroy']); 
+        Route::resource('projekti', ProjectController::class);
     });
 
     Route::prefix('kupac')->group(function () {
         Route::get('/projekti', [ProjectController::class, 'index']); 
         Route::get('/projekti/{id}', [ProjectController::class, 'show']); 
         Route::get('/projekti/pretraga', [ProjectController::class, 'search']); 
+
+        Route::post('/zahtevi/{projectId}', [RequestController::class, 'store']); 
+        Route::put('/zahtevi/{id}', [RequestController::class, 'update']); 
+        Route::patch('/zahtevi/{id}/status', [RequestController::class, 'updateStatus']); 
+        Route::delete('/zahtevi/{id}', [RequestController::class, 'destroy']); 
+
+        Route::post('/recenzije/{requestId}', [ReviewController::class, 'store']); 
+        Route::put('/recenzije/{id}', [ReviewController::class, 'update']); 
+        Route::delete('/recenzije/{id}', [ReviewController::class, 'destroy']); 
     });
     
     Route::post('/logout', [AuthController::class, 'logout']);
